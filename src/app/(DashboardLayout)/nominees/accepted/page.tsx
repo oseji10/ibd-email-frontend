ba19e341049f9f27ba0a6888bd68a48b22ceff3c
Victor Oseji <vctroseji@gmail.com>
2024-09-17 16:01:35 +0100

    updates

--- src/app/(DashboardLayout)/nominees/accepted/page.tsx
+++ src/app/(DashboardLayout)/nominees/accepted/page.tsx
@@ -1,21 +1,16 @@
-'use client';
-import { Typography } from '@mui/material';
-import PageContainer from '@/app/(DashboardLayout)/components/container/PageContainer';
-import DashboardCard from '@/app/(DashboardLayout)/components/shared/DashboardCard';
-import NomineesTable from '../../components/tables/NomineesTable';
-import ProductPerformance from '../../components/dashboard/ProductPerformance';
-import Nominees from '../../components/tables/Nominees';
 
 
-const SamplePage = () => {
+const Accepted = () => {
   return (
-    <PageContainer title="Nominees" description="List of all nominees">
-      <DashboardCard >
+    <>
+    <h4>JJ</h4>
+    
+
         <h5>Thank you Accepting This nomination. Kindly reach out to the Registrar/CEO: Dr. Paul Ikile on 08034520370</h5>
-      </DashboardCard>
-    </PageContainer>
+    </>
+    
   );
 };
 
-export default SamplePage;
+export default Accepted;
 
---
+++ src/app/accepted/page.tsx
@@ -0,0 +1,13 @@
+
+
+const Accepted = () => {
+  return (
+    
+
+        <h3>Thank you Accepting This nomination. Kindly reach out to the Registrar/CEO: Dr. Paul Ikile on 08034520370. <a href="https://ibd.ng">Click here to visit website and make payment</a></h3>
+      
+  );
+};
+
+export default Accepted;
+
